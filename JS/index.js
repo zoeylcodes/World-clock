819c96ebc743ece09a5bf50bef6db459c0ff009f
Zoey <124487432+zoeylcodes@users.noreply.github.com>
2024-07-20 23:53:52 -0400

    updated JS

--- JS/index.js
+++ JS/index.js
@@ -43,6 +43,9 @@ torontoTimeElement.innerHTML = torontoTime.format("h:mm:ss [<small>]A[</small>]"
 
 function updateCity(event){
    let cityTimeZone= event.target.value;
+   if (cityTimeZone === 'current'){
+    cityTimeZone= moment.tz.guess();
+   }
    let cityName=cityTimeZone.replace("_"," ").split("/")[1];
    let cityTime = moment().tz(cityTimeZone);
    let citiesElement= document.querySelector("#cities");
